bde8b840fec847457222fb3f596146288b8c3af9
arf20 <aruizfernandez05@gmail.com>
2024-04-18 03:56:01 +0200

    Read file and call assembler

--- main.c
+++ main.c
@@ -1,9 +1,49 @@
+/*
+
+    arfminesweeper: Cross-plataform multi-frontend game
+    Copyright (C) 2023 arf20 (Ángel Ruiz Fernandez)
+
+    This program is free software: you can redistribute it and/or modify
+    it under the terms of the GNU General Public License as published by
+    the Free Software Foundation, either version 3 of the License, or
+    (at your option) any later version.
+
+    This program is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+    GNU General Public License for more details.
+
+    You should have received a copy of the GNU General Public License
+    along with this program.  If not, see <https://www.gnu.org/licenses/>.
+
+    main.c: Program entry point
+
+*/
+
 #include <stdio.h>
+
+#include "assembler.h"
 
 void
 usage(char *name) {
     printf("Usage: %s [options] file\nOptions\n"
     "  -v\t\tVerbose output.\n  -g\t\tGenerate debug symbols for arfmipssim.\n  -o <file>\tPlace the output into <file>.\n", name);
+}
+
+char *
+read_whole_file(const char *fn) {
+    FILE *f = fopen(fn, "r");
+
+    fseek(f, 0, SEEK_END);
+    size_t ins = ftell(f);
+    fseek(f, 0, SEEK_SET);
+
+    char *buff = malloc(ins);
+    fread(buff, ins, 1, f);
+
+    fclose(f);
+
+    return buff;
 }
 
 int
@@ -16,8 +56,8 @@ main(int argc, char **argv) {
     /* Command line options */
     int verbose = 0;
     int debugsym = 0;
-    char *output = NULL;
-    char *input = NULL;
+    char *outfn = NULL;
+    char *infn = NULL;
 
     for (int i = 1; i < argc; i++) {
         if (argv[i][0] == '-') {
@@ -25,10 +65,10 @@ main(int argc, char **argv) {
             switch (argv[i][1]) {
                 case 'v': verbose = 1; break;
                 case 'g': debugsym = 1; break;
-                case 'o': output = argv[++i]; break;
+                case 'o': outfn = argv[++i]; break;
             }
         } else {
-            if (input == NULL) input = argv[i];
+            if (infn == NULL) infn = argv[i];
             else {
                 usage(*argv);
                 return 1;
@@ -36,12 +76,17 @@ main(int argc, char **argv) {
          }
     }
 
-    if (input == NULL) {
+    if (infn == NULL) {
         usage(*argv);
         return 1;
     }
 
-    printf("v: %d, g: %d, o: %s, i: %s\n", verbose, debugsym, output, input);
+    /* Read input file */
+    char *input = read_whole_file(infn);
+
+    /* Assemble input */
+    segment_t *segments = NULL;
+    assemble(input, &segments, stderr);
 
     return 0;
 }
